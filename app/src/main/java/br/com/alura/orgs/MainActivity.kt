a4efd9e731cd1cd076a7de0c835e938d8387824f
Raphael Bravim <raphael.bravim.ctr@i-systems.com>
2023-08-16 20:31:36 -0300

    aula 13 - executando app no dispositivo

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -1,6 +1,13 @@
 package br.com.alura.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
 
 class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
+    }
 }
